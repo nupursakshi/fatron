5d5bac5a695d8ca897835d2ecde978e7627e3c29
nupursakshi <nupurmohod14@gmail.com>
2025-08-14 10:55:32 +0530

    Add basic Angular component stubs for initial app build and routing

---
+++ frontend/src/app/components/notification/notification.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: `<h2>Notifications</h2><div>Your notifications will appear here.</div>`
+})
+export class NotificationComponent {}
