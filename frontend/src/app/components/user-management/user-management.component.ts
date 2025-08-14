7035476b8baa154104ddf966678b5e498e0936f2
nupursakshi <nupurmohod14@gmail.com>
2025-08-14 10:56:28 +0530

    Add basic Angular component stubs for initial app build and routing

---
+++ frontend/src/app/components/user-management/user-management.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-management',
+  template: `<h2>User Management</h2><div>Manage users here.</div>`
+})
+export class UserManagementComponent {}
