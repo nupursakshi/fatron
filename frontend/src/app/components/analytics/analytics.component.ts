86b25c30df780d8fe80ded5a70a41c423e71fb1b
nupursakshi <nupurmohod14@gmail.com>
2025-08-14 10:55:09 +0530

    Add basic Angular component stubs for initial app build and routing

---
+++ frontend/src/app/components/analytics/analytics.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics',
+  template: `<h2>Analytics</h2><div>View analytics here.</div>`
+})
+export class AnalyticsComponent {}
