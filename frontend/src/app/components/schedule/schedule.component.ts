adfbb9b9291e03957fd466405e7b51fea23db15a
nupursakshi <nupurmohod14@gmail.com>
2025-08-14 10:55:20 +0530

    Add basic Angular component stubs for initial app build and routing

---
+++ frontend/src/app/components/schedule/schedule.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule',
+  template: `<h2>Schedule</h2><div>Audit schedule management.</div>`
+})
+export class ScheduleComponent {}
