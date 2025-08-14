f1e57e5ebadbf799c870bc100f267158c084d260
nupursakshi <nupurmohod14@gmail.com>
2025-08-14 10:56:16 +0530

    Add basic Angular component stubs for initial app build and routing

---
+++ frontend/src/app/components/client-management/client-management.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-management',
+  template: `<h2>Client Management</h2><div>Manage clients here.</div>`
+})
+export class ClientManagementComponent {}
