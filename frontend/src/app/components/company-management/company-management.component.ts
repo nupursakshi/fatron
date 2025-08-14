e1cf8be703936f0a100ea9276dbaae3438b7355b
nupursakshi <nupurmohod14@gmail.com>
2025-08-14 10:56:52 +0530

    Add basic Angular component stubs for initial app build and routing

---
+++ frontend/src/app/components/company-management/company-management.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-management',
+  template: `<h2>Company Management</h2><div>Manage companies here.</div>`
+})
+export class CompanyManagementComponent {}
